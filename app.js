17482fef45d11ceeb6ba4308b1db14dd12501f37
sawyehtet <sawyehtet845@gmail.com>
2023-05-15 14:36:59 +0630

    Streams and Buffers

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-const PizzaShop = require('./pizza-shop');
-const DrinkMachine = require('./drink-machine')
+const buffer = new Buffer.from("Saw Ye Htet");
 
-const pizzaShop = new PizzaShop;
-const drinkMachine = new DrinkMachine;
+buffer.write("code evolution")
 
-pizzaShop.on("order", (size, topping) => {
-    console.log(`Order received! Banking a ${size} pizza with ${topping}`);
-    drinkMachine.serveDrink(size);
-})
-
-pizzaShop.order("large", "mushrooms");
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require("node:events");
-
-// const emitter = new EventEmitter();
-
-// emitter.on("order-pizza",(size,topping) => {
-//     console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-// });
-
-// emitter.on("order-pizza", (size) => {
-//     if(size == "large") {
-//         console.log("Serving complimentary drink!");
-//     }
-// });
-
-// console.log("Do work before event occurs in the system");
-
-// emitter.emit("order-pizza","large", "mushroom")
+console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());
