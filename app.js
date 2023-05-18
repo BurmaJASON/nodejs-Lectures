b957cf92551e9f30f53ddd1c7bdd972165cc4c7c
sawyehtet <sawyehtet845@gmail.com>
2023-05-18 13:29:12 +0630

    Http Routing (req.url)

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
 
-    const name = "Burma";
-    res.writeHead(200,{"Content-Type" : "text/html"});
+    if(req.url === "/") {
+        res.writeHead(200, {"Content-Type" : "text/plain" });
+        res.end("Home Page");
+    }else if(req.url === "/about") {
+        res.writeHead(200, {"Content-type" : "text/plain"});
+        res.end("About Page");
+    }else if(req.url === "/api") {
+        res.writeHead(200, {"Content-Type" : "application/json"});
+        res.end(JSON.stringify({
+            firstName : "Burma",
+            lastName : "Jason"
+        }))
+    }else {
+        res.writeHead(404);
+        res.end("Page Not Found")
+    }
    
-    let html = fs.readFileSync('./index.html',"utf-8");
-    html = html.replace("{{name}}", name);
-    res.end(html);
 })
 
 
