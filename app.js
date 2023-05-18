2bf43e4b9138415bd9de690512b962d326166aa2
sawyehtet <sawyehtet845@gmail.com>
2023-05-18 15:44:46 +0630

    nodejs use libuv for async ,This is ThreadPool from lubuv

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const http = require("node:http");
-const fs = require("node:fs");
-
-const server = http.createServer((req, res) => {
-
-    if(req.url === "/") {
-        res.writeHead(200, {"Content-Type" : "text/plain" });
-        res.end("Home Page");
-    }else if(req.url === "/about") {
-        res.writeHead(200, {"Content-type" : "text/plain"});
-        res.end("About Page");
-    }else if(req.url === "/api") {
-        res.writeHead(200, {"Content-Type" : "application/json"});
-        res.end(JSON.stringify({
-            firstName : "Burma",
-            lastName : "Jason"
-        }))
-    }else {
-        res.writeHead(404);
-        res.end("Page Not Found")
-    }
-   
-})
+const crypto = require("node:crypto");
 
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+const MAX_CALLS = 3;
+
+const start = Date.now();
+for(let i =0; i < MAX_CALLS; i++) {
+    crypto.pbkdf2("password", "salt", 100000, 512, "sha512" , () => {
+        console.log(`Hash: ${i+1}`,Date.now() - start);
+    })
+}
+
+
+console.log("hello async");
+
+
+// const start = Date.now();
+// crypto.pbkdf2Sync("password", "salt" , 100000, 512, "sha512");
+// crypto.pbkdf2Sync("password", "salt" , 100000, 512, "sha512");
+
+
+// console.log("Hash: ", Date.now() - start);
+
+// console.log("Hello sync");
