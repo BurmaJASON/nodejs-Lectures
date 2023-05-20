47455e3b82cf16722951360a76f03adc3ba453a2
sawyehtet <sawyehtet845@gmail.com>
2023-05-21 00:07:18 +0630

    experiments for I/O Queue

--- app.js
+++ app.js
@@ -1,72 +1,49 @@
-//Experiment 5
-setTimeout(() => console.log("this is setTimeout 1"),1000); 
-setTimeout(() => console.log("this is setTimeout 2"),500); 
-setTimeout(() => console.log("this is setTimeout 3"),0); 
+// Experiment 8 
+const fs = require("fs");
+fs.readFile(__filename, () => {
+    console.log("this is readFile 1");
+})
+
+process.nextTick(() => console.log("this is process.nextTick 1"));
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
+setTimeout(() => console.log("this is setTimeout 1"),0);
+
+console.log("sync");
 
 
 
 
 
 
+// Experiment 7
 
-// Experiment 4
-// setTimeout(() => console.log("this is setTimeout 1"),0); 
-// setTimeout(() => {
-//     process.nextTick(() => {
-//         console.log("this is the inner next tick  inside setTimeout");
-//     })
-//     console.log("this is setTimeout 2");
-// },0);
-// setTimeout(() => console.log("this is setTimeout 3"),0);
+// The Order of exceution can never be guarnateed with delay 0ms
+// const fs = require("fs");
 
+// setTimeout(() => console.log("this is setTimeout 1"),0);
 
-// process.nextTick(()=> console.log('this is process.nextTick 1'));
-// process.nextTick(()=> {
-//     console.log('this is process.nextTick 2');
-//     process.nextTick(() => {
-//         console.log('this is the inner next tick inside next tick');
-//     })
-// });
-// process.nextTick(()=> console.log('this is process.nextTick 3'));
-
-
-
-
-// Promise.resolve().then(() => console.log("this is Promise.resolve() 1"));
-// Promise.resolve().then(() => {
-//     console.log("this is Promise.resolve() 2");
-//     process.nextTick(()=> {
-//         console.log("this is the inner next tick inside Promise then block");
-//     })
+// fs.readFile(__filename, () => {
+//     console.log("this is read file 1");
+//     process.nextTick(() => console.log("this is process.nextTick 1"))
 // })
-// Promise.resolve().then(() => console.log("this is Promise.resolve() 3"));
-
-
-
-
-
-// Experiment 3 
-// setTimeout(() => console.log("this is setTimeout 1"),0); 
-// setTimeout(() => console.log("this is setTimeout 2"),0); 
-// setTimeout(() => console.log("this is setTimeout 3"),0); 
 
 
 
 
 
 
-// Experiment 2
-
-// Promise.resolve().then(() => console.log("this is Promise.resolve() 1"));
-// process.nextTick(()=> console.log('this is process.nextTick 1'))
-// console.log("Sync");
 
 
 
-// Experiment 1
+// Experiment 6
 
-// console.log('consolelog 1');
-// process.nextTick(()=> console.log("this is process.nextTick 1"))
-// console.log('consolelog 2');
+// const fs = require("fs");
 
+// fs.readFile(__filename, () => {
+//     console.log("This is readFile");
+// })
 
+// process.nextTick(() => console.log("this is process.nextTick 1"))
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+
+// console.log("sync");
