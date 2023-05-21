4588a362f290b00c5ee77482b4e82d1523d56b5b
sawyehtet <sawyehtet845@gmail.com>
2023-05-21 23:33:18 +0630

    close queue and summary note for event loop

--- app.js
+++ app.js
@@ -1,90 +1,25 @@
+const fs = require("fs");
+
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+
+readableStream.on("close", () => {
+    console.log("this is form readableStream close event callback");
+})
+
+setImmediate(() => console.log("this is setIntermediate 1"));
 setTimeout(() => console.log("this is setTimeout 1"),0)
-setImmediate(() => console.log("this is setImmediate 1") )
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
+process.nextTick(() => console.log("this is process.nextTick"))
 
 
 
+// Summary note for event loop
 
-// Experiment 11
-// setImmediate(() => console.log('this is setImmediate 1'));
-// setImmediate(() => {
-//     console.log('this is setImmediate 2');
-//     process.nextTick(() => console.log("this is process.nextTick 1"));
-//     Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
-// })
-// setImmediate(() => console.log("this is setImmediate 3"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Experiment 10 
-// const fs = require("fs");
-// fs.readFile(__filename, () => {
-//     console.log("this is readFile 1");
-//     setImmediate(() => console.log("this is inner setImmediate inside readFile "));
-//     process.nextTick(() => console.log("this is process.nextTick inside readFile"));
-//     Promise.resolve().then(() => console.log("this is Promise.resolve inside readFile"))
-
-// })
-
-// process.nextTick(() => console.log("this is process.nextTick 1"));
-// Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
-// setTimeout(() => console.log("this is setTimeout 1"),0);
-// setImmediate(() => console.log("this is setImmediate 1"))
-
-
-// console.log("sync");
-
-
-
-
-
-
-// Experiment 7
-
-// The Order of exceution can never be guarnateed with delay 0ms
-// const fs = require("fs");
-
-// setTimeout(() => console.log("this is setTimeout 1"),0);
-
-// fs.readFile(__filename, () => {
-//     console.log("this is read file 1");
-//     process.nextTick(() => console.log("this is process.nextTick 1"))
-// })
-
-
-
-
-
-
-
-
-
-// Experiment 6
-
-// const fs = require("fs");
-
-// fs.readFile(__filename, () => {
-//     console.log("This is readFile");
-// })
-
-// process.nextTick(() => console.log("this is process.nextTick 1"))
-// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-
-// console.log("sync");
+// 1.We use process.nextTick() method to queue into the nexttick queue
+// 2.We resolve or reject a Promise to queue into the Promise queue
+// 3.We use setTimeout or setInterval to queue into th e timer queue
+// 4.Execute an async method to queue into the I/O queue
+// 5. User setImmediate funtion to queue into the check queue 
+// 6. Attach close event listeners to queue into the close queue
