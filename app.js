0ed54ee65847fc5a53162a5c118435b80e79d45c
sawyehtet <sawyehtet845@gmail.com>
2023-05-21 13:17:54 +0630

    Check Queue

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-// Experiment 8 
-const fs = require("fs");
-fs.readFile(__filename, () => {
-    console.log("this is readFile 1");
-})
+setTimeout(() => console.log("this is setTimeout 1"),0)
+setImmediate(() => console.log("this is setImmediate 1") )
 
-process.nextTick(() => console.log("this is process.nextTick 1"));
-Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
-setTimeout(() => console.log("this is setTimeout 1"),0);
-setImmediate(() => console.log("this is setImmediate 1"))
 
-console.log("sync");
+
+
+// Experiment 11
+// setImmediate(() => console.log('this is setImmediate 1'));
+// setImmediate(() => {
+//     console.log('this is setImmediate 2');
+//     process.nextTick(() => console.log("this is process.nextTick 1"));
+//     Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
+// })
+// setImmediate(() => console.log("this is setImmediate 3"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Experiment 10 
+// const fs = require("fs");
+// fs.readFile(__filename, () => {
+//     console.log("this is readFile 1");
+//     setImmediate(() => console.log("this is inner setImmediate inside readFile "));
+//     process.nextTick(() => console.log("this is process.nextTick inside readFile"));
+//     Promise.resolve().then(() => console.log("this is Promise.resolve inside readFile"))
+
+// })
+
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"))
+// setTimeout(() => console.log("this is setTimeout 1"),0);
+// setImmediate(() => console.log("this is setImmediate 1"))
+
+
+// console.log("sync");
 
 
 
