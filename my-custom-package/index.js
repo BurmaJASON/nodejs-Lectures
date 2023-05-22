d5671689e60d15ed306365e3091a76cf86274380
sawyehtet <sawyehtet845@gmail.com>
2023-05-22 23:40:02 +0630

    using packages

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,5 +1,11 @@
-function greet(name) {
-    console.log(`Hello ${name}, welcom to Burrma`);
-}
+// const localModule = require("./path to localModule")
+// const fs = require("fs");
 
+
+const upperCase = require("upper-case").upperCase;
+
+function greet(name) {
+    console.log(upperCase(`Hello ${name}, welcom to Burrma`));
+}
+greet("burmajason")
 module.exports = greet;
