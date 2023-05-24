c286d9c018da541532a1283f61f0557dca27faa7
sawyehtet <sawyehtet845@gmail.com>
2023-05-24 22:10:58 +0630

    event emitter

---
+++ event.js
@@ -0,0 +1,14 @@
+const event = require('events');
+
+const e = new event.EventEmitter();
+
+e.on('like',() => {
+    console.log('like this');
+})
+
+e.on('dislike',() => {
+    console.log('dislike this');
+})
+
+e.emit("like");
+e.emit("dislike")
