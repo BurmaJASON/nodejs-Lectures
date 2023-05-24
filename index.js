d61ac148583cd0a45f2ea9ff219bfc0f879af2ce
sawyehtet <sawyehtet845@gmail.com>
2023-05-24 21:26:44 +0630

    render html document

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require("node:http")
 
+const fs = require("node:fs")
+
 http.createServer((req,res) => {
-    res.end("hello nodejs")
+    fs.readFile('./page/index.html',(err,data)  => {
+        res.write(data);
+        res.end()
+    })
 }).listen(4000,() => console.log('server running on port 4000'))
