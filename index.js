2b2b31ee5b0b209820d3d0a46464dccbd4aff742
sawyehtet <sawyehtet845@gmail.com>
2023-05-24 21:42:19 +0630

    create first static web page

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const http = require("node:http")
 const fs = require("node:fs")
 
 http.createServer((req,res) => {
-    fs.readFile('./page/index.html',(err,data)  => {
-        res.write(data);
-        res.end()
-    })
+    const url = req.url;
+    if(url == "/") {
+        fs.readFile('./page/index.html',(err,data)=> {
+            res.write(data);
+            res.end();
+        })
+    }else if (url == "/about") {
+        fs.readFile('./page/about.html',(err,data)=> {
+            res.write(data);
+            res.end()
+        })
+    }else {
+        fs.readFile('./page/contact.html',(err,data) => {
+            res.write(data);
+            res.end();
+        })
+    }
 }).listen(4000,() => console.log('server running on port 4000'))
