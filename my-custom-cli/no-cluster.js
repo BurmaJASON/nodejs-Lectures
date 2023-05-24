679c798075f0beec3e4a9ac961d5091072607a52
sawyehtet <sawyehtet845@gmail.com>
2023-05-24 12:48:43 +0630

    Cluster Module

---
+++ my-custom-cli/no-cluster.js
@@ -0,0 +1,14 @@
+const http = require("http")
+
+const server = http.createServer((req,res) => {
+    if(req.url === "/") {
+        res.writeHead(200, {"Content-Type" : "text/plain"});
+        res.end("Home Page");
+    }else if (req.url === "/slow-page") {
+        for (let i = 0; i < 60000000; i++) {}
+        res.writeHead(200, {"Content-Type" : "text/plain"});
+        res.end("Slow Page");
+    }
+});
+
+server.listen(8000, () => console.log("server is running on port 8000"));
