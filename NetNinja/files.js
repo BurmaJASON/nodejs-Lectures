0848606f36be19b0dc5766149603fb9152a3376c
sawyehtet <sawyehtet845@gmail.com>
2023-05-28 20:59:31 +0630

    writing file fs.writeFile

--- NetNinja/files.js
+++ NetNinja/files.js
@@ -3,18 +3,26 @@ const fs = require("node:fs")
 // reading files
 
     //async readFile
-    fs.readFile('./docs/blog1.txt',(err,data)   => {
-        if(err) console.log(err);
-        // console.log(data);//this will give buffer of the string
-        console.log(data.toString());
-        console.log("file is read");
-    });
-    console.log('last line');
+    // fs.readFile('./docs/blog1.txt',(err,data)   => {
+    //     if(err) console.log(err);
+    //     // console.log(data);//this will give buffer of the string
+    //     console.log(data.toString());
+    //     console.log("file is read");
+    // });
+    // console.log('last line');
 
 
 
 // writing files
+    // overwriting a file
+    fs.writeFile('./docs/blog1.txt', 'hello world', () => {
+        console.log('file is overwritten');
+    });
 
+    // writing a new file
+    fs.writeFile('./docs/blog2.txt', 'hello jason', () => {
+        console.log('file is created and written');
+    });
 
 
 
