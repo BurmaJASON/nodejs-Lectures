9d2eb16c2a040532c0c7d1e5b7247f1bda129369
sawyehtet <sawyehtet845@gmail.com>
2023-05-29 15:00:56 +0630

    writeStream

--- NetNinja/streams.js
+++ NetNinja/streams.js
@@ -3,8 +3,13 @@ const fs = require('fs');
 
 // Read stream
 const readStream = fs.createReadStream('./docs/blog3.txt',{encoding: 'utf-8'});
+// Write steram
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
 
 readStream.on('data',(chunk)=> {
     console.log('------NEW CHUNK------');
     console.log(chunk);
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 })
