23278556f98a228cad536cc2db84c2bf28aeea07
sawyehtet <sawyehtet845@gmail.com>
2023-05-30 16:13:15 +0630

    routing with req.url

--- NetNinja/server.js
+++ NetNinja/server.js
@@ -3,10 +3,23 @@ const fs = require('node:fs')
 
 const server = http.createServer((req,res)=> {
     // set header content type
-    res.setHeader('Content-Type','text/html')
+    res.setHeader('Content-Type','text/html');
+    let path = './views/';
+
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default :
+            path += '404.html'
+            break;
+    }
 
     // send an html file
-    fs.readFile('./views/index.html',(err,data)=> {
+    fs.readFile(path,(err,data)=> {
         if(err) {
             console.log(err);
             res.end();
