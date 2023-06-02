1102a58da1d091a91244f5cbe00dfa57ddbf94cb
sawyehtet <sawyehtet845@gmail.com>
2023-06-02 16:02:05 +0630

    ejs view engine

--- NetNinja/app.js
+++ NetNinja/app.js
@@ -4,29 +4,31 @@ const express = require('express');
 const app = express();
 
 
+// register view engine
+app.set('view engine', 'ejs');//if your render file is under views folder it will automatically render that file but its is under others u will need to config that file like below codes
+// app.set('views','myviews');
+
+
 // listen for requests
 app.listen(3000);
 
 
 app.get('/',(req,res) => {
-    // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname })
+    
+    res.render('index');
 })
 
 
 app.get('/about',(req,res) => {
-    // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about');
 })
 
 
-// redirects
-app.get('/about-us', (req,res)  => {
-    res.redirect('/about');
+app.get('/blogs/create', (req,res) => {
+    res.render('create');
 })
-
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root : __dirname})
+    res.status(404).render('404')
 })
