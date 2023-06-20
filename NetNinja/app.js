85fa01654e1a9a241f7f0fc711c0914008f188a7
sawyehtet <sawyehtet845@gmail.com>
2023-06-20 16:21:23 +0630

    outputting data in the view file

--- NetNinja/app.js
+++ NetNinja/app.js
@@ -27,40 +27,11 @@ app.use(morgan('dev'))
 
 // mongoose and mongo sandbox routes
 
-app.get('/add-blog',(req,res) => {
-    const blog = new Blog({
-        title : 'new blog2',
-        snippet : 'about my new blog',
-        body: 'more about my new blog'
-    });
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch(err => console.log(err))
-})
 
 
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => res.send(result) )
-        .catch(error => console.log(error))
-})
-
-app.get('/single-blog', (req,res) => {
-    Blog.findById('64917086eb55750a55dd3270')
-        .then((result) => res.send(result))
-        .catch(err => console.log(err))
-})
-
+//routes
 app.get('/',(req,res) => {
-    const blogs = [
-        {title: 'Saw finds eggs', snippet: 'Lorem ipsum dolor sit  consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit  consectetur'},
-        {title: 'How to defeat browser', snippet: 'Lorem ipsum dolor sit  consectetur'},
-    ];
-    
-    res.render('index', {title: 'Home', blogs });
+    res.redirect('/blogs');
 })
 
 
@@ -68,6 +39,15 @@ app.get('/about',(req,res) => {
     res.render('about', {title: 'About'});
 })
 
+//blog routes
+app.get('/blogs',(req,res) => {
+    // get all blogs from database\
+    Blog.find().sort({createdAt: -1})
+        .then(result => {
+            res.render('index', {title: 'All Blogs', blogs : result})
+        })
+        .catch(err => console.log(err))
+})
 
 app.get('/blogs/create', (req,res) => {
     res.render('create', {title: 'Create a new Blog'});
