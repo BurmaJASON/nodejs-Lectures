a5e070c34006d35acfdd94f36b2608cbfb37ccb3
sawyehtet <sawyehtet845@gmail.com>
2023-06-21 16:39:00 +0630

    route parameters

--- NetNinja/app.js
+++ NetNinja/app.js
@@ -59,6 +59,15 @@ app.post('/blogs',(req,res) => {
         .catch((err) => console.log(err))
 })
 
+app.get('/blogs/:id',(req,res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('detail', {blog: result, title : 'Blog Details'})
+        })
+        .catch(err => console.log(err))
+})
+
 app.get('/blogs/create', (req,res) => {
     res.render('create', {title: 'Create a new Blog'});
 })
